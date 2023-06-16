1809fc6ba0f8a565c80cc6e749652ea091bd3989
Yini <104151346+ElaineLi0320@users.noreply.github.com>
2023-06-16 08:42:05 +0800

    Add files via upload

---
+++ backend/src/main/java/tech/getarrays/employee/manager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package tech.getarrays.employee.manager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.employee.manager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
+
+}
